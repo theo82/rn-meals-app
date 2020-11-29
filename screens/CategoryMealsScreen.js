f4885231487e4fee6e67616e2fed2503955c1c9e
theo82 <theoaristi53@gmail.com>
2020-11-29 09:43:56 +0200

    Rendering a Meals List

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,21 +3,34 @@ import {View, FlatList, Text, StyleSheet } from 'react-native';
 import { HeaderTitle } from 'react-navigation-stack';
 import { CATEGORIES, MEALS} from '../data/dummy-data';
 import CategoriesScreen from './CategoriesScreen';
+import  MealItem from '../components/MealItem';
 
 const CategoryMealScreen = props => {
 
     const renderMealItem = (itemData) => {
-        return(
-        <View><Text>{itemData.item.title}</Text></View>
-        )
-    }
+        return (
+            <MealItem 
+                title={itemData.item.title} 
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {}} 
+            />
+        );
+    };
+
     const catId = props.navigation.getParam('categoryId');
-    const selectCategory = CATEGORIES.find(cat => cat.id ===catId)
 
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
     return (
         <View style={styles.screen}>
-           <FlatList data={displayedMeals} keyExtractor={(item,index)=> item.id} renderItem={renderMealItem} />
+           <FlatList 
+            data={displayedMeals} 
+            keyExtractor={(item,index)=> item.id} 
+            renderItem={renderMealItem}
+            style={{width: '100%'}} 
+        />
         </View>
 
     )
@@ -36,7 +49,8 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding: 15
     }
 })
 
