c8800b3e9404556a898740ac0e1fbe796f9c64e2
theo82 <theoaristi53@gmail.com>
2020-12-04 16:51:47 +0200

    Adding Filtering Logic

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,5 +1,7 @@
- import { MEALS } from '../../data/dummy-data';
+import { MEALS } from '../../data/dummy-data';
 import { TOGGLE_FAVORITE } from '../actions/meals';
+import { SET_FILTERS } from '../actions/meals';
+
 const initialState = {
     meals: MEALS,
     filteredMeals: MEALS,
@@ -7,23 +9,45 @@ const initialState = {
 };
 
 const mealsReducers = (state = initialState, action) => {
-    switch(action.type) {
-        case TOGGLE_FAVORITE: {
-            const existingIndex = state.favoriteMeals.findIndex(
-                meal => meal.id === action.mealId
-            );
-            if(existingIndex >= 0) {
-                const updatedFavMeals = [...state.favoriteMeals];
-                updatedFavMeals.splice(existingIndex, 1);
-                return { ...state, favoriteMeals: updatedFavMeals };
-            } else {
-                const meal = state.meals.find(meal => meal.id === action.mealId);
-                
-                return {...state, favoriteMeals: state.favoriteMeals.concat(meal)};
+        switch(action.type) {
+            case TOGGLE_FAVORITE: {
+                const existingIndex = state.favoriteMeals.findIndex(
+                    meal => meal.id === action.mealId
+                );
+                if(existingIndex >= 0) {
+                    const updatedFavMeals = [...state.favoriteMeals];
+                    updatedFavMeals.splice(existingIndex, 1);
+                    return { ...state, favoriteMeals: updatedFavMeals };
+                } else {
+                    const meal = state.meals.find(meal => meal.id === action.mealId);
+                    
+                    return {...state, favoriteMeals: state.favoriteMeals.concat(meal)};
+                }
             }
+            case SET_FILTERS:
+            const appliedFilters = actions.filters;
+            const updatedFilteredMeals = state.meals.filter(meal => {
+                if(appliedFilters.gluttenFree && !meal.gluttenFree) {
+                    return false;
+                }
+                if(appliedFilters.lactoseFree && !meal.lactoseFree) {
+                    return false;
+                }
+                if(appliedFilters.vegetarian && !meal.isVegetarian) {
+                    return false;
+                }
+                if(appliedFilters.isVegan && !meal.isVegan) {
+                    return false;
+                }
+
+                return true;
+            });
+            return{ ...state, filteredMeals: updatedFilteredMeals}
+        
+        default: {
+            return state;
         }
     }
-    return state;
 }
 
 export default mealsReducers;
