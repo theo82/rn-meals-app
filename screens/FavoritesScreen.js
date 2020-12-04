1634f9f061d6f023bffb5df74d5a9ac453a34157
theo82 <theoaristi53@gmail.com>
2020-12-04 16:13:48 +0200

    Rendering a Fallback Text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'; 
 import MealList from '../components/MealList';
 import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -7,6 +8,11 @@ const FavoritesScreen = props => {
 
     const favMeals = useSelector(state => state.meals.favoriteMeals);
 
+    if(favMeals.length === 0 || !favMeals) {
+        return <View style={styles.content}>
+            <Text>No favorite meals found. Start adding some</Text>
+        </View>
+    } 
 
     return <MealList listData ={favMeals} navigation={props.navigation}/>
 };
@@ -31,6 +37,14 @@ FavoritesScreen.navigationOptions = navData => {
         ) 
     }       
   };
+
+  const styles = StyleSheet.create({
+      content: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+      }
+  })
 
 
 
